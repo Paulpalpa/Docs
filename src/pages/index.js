21f9ca7b837830a80dad9f1811a6edf96f4c30b9
刘漾鹏 <YANGPENG.LIU@HAND-CHINA.COM>
2022-12-26 11:43:15 +0800

    [REF] 标题更新

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import ThreeComponent from '@site/src/components/ThreeComponent';
 
-
-import styles from './index.module.css';
-
-// function HomepageHeader() {
-//   const {siteConfig} = useDocusaurusContext();
-//   return (
-//     <header className={styles.heroBanner}>
-//       <div className="container">
-//         <h1 className="hero__title">{`前端记录小站`}</h1>
-//         <div className={styles.buttons}>
-//           <Link
-//             className="button button--secondary button--lg"
-//             to="/docs/intro">
-//            开始浏览 ⏱️
-//           </Link>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Paul的前端小站`}>
       <ThreeComponent />
       <main>
         <HomepageFeatures />
